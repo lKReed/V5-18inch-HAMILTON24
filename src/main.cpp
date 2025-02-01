bdb179cbfa0a34e426dbd30156d2c391faaafc98
lKReed <lkreed1757@gmail.com>
2025-02-01 07:44:06 -0500

    Pre-Comp Push

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ motor intake = motor(PORT2, true);
 
 motor conveyer = motor(PORT20, false);
 
-motor clamp = motor(PORT5, false);
+motor clamp = motor(PORT14, false);
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -45,6 +45,9 @@ motor clamp = motor(PORT5, false);
 void pre_auton(void) {
 
   std::cout << "IN PRE-AUTO\n";
+
+  intake.setVelocity(70, rpm);
+  conveyer.setVelocity(70, rpm);
 
 }
 
@@ -78,7 +81,7 @@ void tankDrive() {
   if (Controller.Axis2.position() < 0)
     velocity2 *= -1;
 
-  double velocity3 = (pow(abs(Controller.Axis2.position()), 1.43) / 1000) * 100;
+  double velocity3 = (pow(abs(Controller.Axis3.position()), 1.43) / 1000) * 100;
   if (Controller.Axis3.position() < 0)
     velocity3 *= -1;
 
@@ -96,13 +99,13 @@ void usercontrol(void) {
     tankDrive();
 
     // Intake + Conveyer
-    if (Controller.ButtonL2.pressing()) {
-      std::cout << "Intake Out\n";
+    if (Controller.ButtonL1.pressing()) {
+      std::cout << "Intake In\n";
       intake.spin(forward);
       conveyer.spin(forward);
     }
-    else if (Controller.ButtonL1.pressing()) {
-      std::cout << "Intake In\n";
+    else if (Controller.ButtonL2.pressing()) {
+      std::cout << "Intake Out\n";
       intake.spin(reverse);
       conveyer.spin(reverse);
     }
@@ -110,7 +113,24 @@ void usercontrol(void) {
       intake.stop();
     }
 
-    // Conveyer
+  // Clamp
+    if (Controller.ButtonR1.pressing()) {
+      std::cout << "Clamp Down\n";
+      clamp.spin(reverse);
+    }
+    else if (Controller.ButtonR2.pressing()) {
+      std::cout << "Clamp Up\n";
+      clamp.spin(forward);
+    }
+
+  // Forward
+  if (Controller.ButtonUp.pressing()) {
+    std::cout << "Forward\n";
+    leftDrive.spin(forward);
+    rightDrive.spin(forward);
+  }
+
+  // Conveyer
     if (Controller.ButtonB.pressing()) {
       std::cout << "Conveyer Down";
       conveyer.spin(forward);
